b65aca811e0e6c95a80642306374bc39fbd1c501
benthepsychologist <benthepsychologist@users.noreply.github.com>
2025-11-26 15:38:20 +0000

    Initial Hugo site for org-1.org

---
+++ README.md
@@ -0,0 +1,2 @@
+# org-1-site
+org-1.org website in markdown, built in markdown with Hugo
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from org-1-site!")
+
+
+if __name__ == "__main__":
+    main()
